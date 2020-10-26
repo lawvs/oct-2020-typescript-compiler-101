1ba3f030af263ab746da47bbdd78fb6a0e3ed5ce
lawvs <18554747+lawvs@users.noreply.github.com>
2020-10-26 23:27:10 +0900

    feat: solve 0-HelloWorld

--- tests/0-HelloWorld/code.ts
+++ tests/0-HelloWorld/code.ts
@@ -1,10 +1,24 @@
-import { Node, SourceFile, TransformerFactory, visitEachChild, VisitResult } from 'typescript'
+import {
+    isStringLiteral,
+    Node,
+    SourceFile,
+    SyntaxKind,
+    TransformerFactory,
+    visitEachChild,
+    VisitResult,
+} from 'typescript'
 
 export default function (): TransformerFactory<SourceFile> {
     return (context) => {
         const { factory } = context
         return (sourceFile) => {
             function visit<T extends Node>(node: T): VisitResult<Node> {
+                // console.log(SyntaxKind[node.kind])
+                if (isStringLiteral(node)) {
+                    // console.log(node.text)
+                    return factory.createStringLiteral('Hello World!', true)
+                }
+
                 return visitEachChild(node, visit, context)
             }
             return visitEachChild(sourceFile, visit, context)
