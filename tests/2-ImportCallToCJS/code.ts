97afc0982fc0c07b02807ac6cc5e75b6b9827b0b
lawvs <18554747+lawvs@users.noreply.github.com>
2020-10-26 23:27:43 +0900

    feat: solve 2-ImportCallToCJS

--- tests/2-ImportCallToCJS/code.ts
+++ tests/2-ImportCallToCJS/code.ts
@@ -1,10 +1,35 @@
-import { Node, SourceFile, TransformerFactory, visitEachChild, VisitResult } from 'typescript'
+import {
+    Node,
+    SourceFile,
+    TransformerFactory,
+    visitEachChild,
+    VisitResult,
+    // @ts-ignore internal api
+    isImportCall,
+    CallExpression,
+} from 'typescript'
 
 export default function (): TransformerFactory<SourceFile> {
     return (context) => {
         return (sourceFile) => {
             const { factory } = context
+
+            function createCommonJSDynamicImport(node: CallExpression) {
+                // Promise.resolve(require('xxxxx'))
+                return factory.createCallExpression(
+                    factory.createPropertyAccessExpression(
+                        factory.createIdentifier('Promise'),
+                        factory.createIdentifier('resolve')
+                    ),
+                    node.typeArguments,
+                    [factory.createCallExpression(factory.createIdentifier('require'), undefined, node.arguments)]
+                )
+            }
+
             function visit<T extends Node>(node: T): VisitResult<Node> {
+                if (isImportCall(node)) {
+                    return createCommonJSDynamicImport((node as unknown) as CallExpression)
+                }
                 return visitEachChild(node, visit, context)
             }
             return visitEachChild(sourceFile, visit, context)
